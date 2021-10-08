82919d65e6e2369a98f51803a0bf0c85f5318106
eborrell <ribera.borrell@fu-berlin.de>
2021-10-08 16:37:04 +0200

    #2 merge one plot and multiple plots

--- src/figures/myfigure.py
+++ src/figures/myfigure.py
@@ -1,5 +1,6 @@
 from matplotlib.figure import Figure
 from matplotlib import rc
+import numpy as np
 
 import os
 
@@ -43,7 +44,7 @@ class MyFigure(Figure):
         self.plot_type = 'linear'
 
         # legend
-        self.legend_loc = 'best'
+        self.legend_loc = 'upper right'
 
     @property
     def file_path(self):
@@ -96,53 +97,43 @@ class MyFigure(Figure):
         assert plot_type in PLOT_TYPES, ''
         self.plot_type = plot_type
 
-    def plot_one_line(self, x, y, color=None, linestyle=None, label=None):
-        assert x.ndim == y.ndim == 1, ''
-        assert x.shape[0] == y.shape[0], ''
-
-        # axes of the figure
-        ax = self.axes[0]
-
-        # plot
-        if self.plot_type == 'linear':
-            ax.plot(x, y, color=color, linestyle=linestyle, label=label)
-        elif self.plot_type == 'semilogx':
-            ax.semilogx(x, y, color=color, linestyle=linestyle, label=label)
-        elif self.plot_type == 'semilogy':
-            ax.semilogy(x, y, color=color, linestyle=linestyle, label=label)
-        elif self.plot_type == 'loglog':
-            ax.loglog(x, y, color=color, linestyle=linestyle, label=label)
-
-        # legend
-        if label is not None:
-            ax.legend(loc=self.legend_loc, fontsize=8)
-
-        # save figure
-        if self.file_path is not None:
-            self.savefig(self.file_path)
-
-    def plot_multiple_lines(self, x, y, colors=None, linestyles=None, labels=None):
+    def plot(self, x, y, colors=None, linestyles=None, labels=None):
         assert x.ndim == 1, ''
-        assert y.ndim == 2, ''
-        assert x.shape[0] == y.shape[1], ''
+        assert y.ndim in [1, 2], ''
+
+        # expand dimension of y if just one array is given
+        if y.ndim == 1:
+            assert x.shape[0] == y.shape[0], ''
+            y = np.expand_dims(y, axis=0)
+        else:
+            assert x.shape[0] == y.shape[1], ''
 
         # number of lines to plot
         n_lines = y.shape[0]
 
         # colors
-        if colors is not None:
+        if colors is not None and type(colors) == str:
+            assert n_lines == 1, ''
+            colors = [colors]
+        elif colors is not None and type(colors) == list:
             assert n_lines == len(colors), ''
         else:
             colors = [None for i in range(n_lines)]
 
         # linestyles
-        if linestyles is not None:
+        if linestyles is not None and type(linestyles) == str:
+            assert n_lines == 1, ''
+            linestyle = [linestyles]
+        elif linestyles is not None and type(linestyles) == list:
             assert n_lines == len(linestyles), ''
         else:
             linestyles = ['-' for i in range(n_lines)]
 
         # labels
-        if labels is not None:
+        if labels is not None and type(labels) == str:
+            assert n_lines == 1, ''
+            labels = [labels]
+        elif labels is not None and type(labels) == list:
             assert n_lines == len(labels), ''
         else:
             labels = [None for i in range(n_lines)]
---
+++ tests/test_plot.py
@@ -0,0 +1,123 @@
+from figures.myfigure import MyFigure
+import matplotlib.pyplot as plt
+
+import numpy as np
+from pathlib import Path
+import os
+import pytest
+
+SOURCE_PATH = Path(os.path.dirname(__file__))
+PROJECT_PATH = SOURCE_PATH.parent
+DATA_PATH = os.path.join(PROJECT_PATH, 'data')
+
+class TestPlot:
+    '''
+    '''
+    @pytest.fixture
+    def dir_path(self):
+        ''' returns dir path for the test plots
+        '''
+        dir_path = os.path.join(DATA_PATH, 'tests')
+        return dir_path
+
+    @pytest.fixture
+    def x(self):
+        ''' returns one-dimensional array
+        '''
+        h = 0.1
+        lb = 0
+        rb = 1
+        x = np.arange(lb, rb + h, h)
+        return x
+
+    @pytest.fixture
+    def f(self, x):
+        ''' returns one-dimensional array
+        '''
+        return x**2
+
+    @pytest.fixture
+    def g(self, x):
+        ''' returns one-dimensional array
+        '''
+        return x**3
+
+    #@pytest.mark.skip(reason='')
+    def test_plot_one_line(self, dir_path, x, f):
+        ''' plot y = x**2
+        '''
+        fig = plt.figure(
+            FigureClass=MyFigure,
+            dir_path=dir_path,
+            file_name='one_line',
+        )
+        fig.plot(x, f)
+
+    def test_plot_one_line_legend(self, dir_path, x, f):
+        '''
+        '''
+        fig = plt.figure(
+            FigureClass=MyFigure,
+            dir_path=dir_path,
+            file_name='one_line_legend',
+        )
+        fig.set_legend_location('lower right')
+        fig.plot(x, f, labels=r'$f(x) = x^2$')
+
+    def test_plot_one_line_linestyle(self, dir_path, x, f):
+        '''
+        '''
+        fig = plt.figure(
+            FigureClass=MyFigure,
+            dir_path=dir_path,
+            file_name='one_line_linestyle',
+        )
+        fig.plot(x, f, linestyles='--')
+
+    def test_plot_one_line_scales(self, dir_path, x, f):
+        '''
+        '''
+        fig = plt.figure(
+            FigureClass=MyFigure,
+            dir_path=dir_path,
+            file_name='one_line_scales',
+        )
+        fig.set_plot_type('loglog')
+        fig.plot(x, f)
+
+    def test_plot_multiple_lines(self, dir_path, x, f, g):
+        '''
+        '''
+        fig = plt.figure(
+            FigureClass=MyFigure,
+            dir_path=dir_path,
+            file_name='multiple_lines',
+        )
+        y = np.vstack((f, g))
+        fig.plot(x, y)
+
+    def test_plot_multiple_lines_legend(self, dir_path, x, f, g):
+        '''
+        '''
+        fig = plt.figure(
+            FigureClass=MyFigure,
+            dir_path=dir_path,
+            file_name='multiple_lines_legend',
+        )
+
+        y = np.vstack((f, g))
+        labels = [r'$f(x) = x^2$', r'$f(x) = x^3$']
+        fig.set_legend_location('upper right')
+        fig.plot(x, y, labels=labels)
+
+    def test_plot_multiples_lines_scales(self, dir_path, x, f, g):
+        '''
+        '''
+        fig = plt.figure(
+            FigureClass=MyFigure,
+            dir_path=dir_path,
+            file_name='multiple_lines_scales',
+        )
+        y = np.vstack((f, g))
+        fig.set_plot_type('loglog')
+        fig.plot(x, y)
